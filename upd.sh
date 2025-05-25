5a1bcb0612e75f8e052301fa67539a256002530b
Vadim Sokolov <vsokolov@Vadims-MacBook-Air.local>
2025-05-25 11:40:44 -0400

    Update files with new changes

--- upd.sh
+++ upd.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # This script updates all files in the current directory with new changes
 
+git pull origin main
 git add .
 git commit -m "Update files with new changes"
 git push origin main
